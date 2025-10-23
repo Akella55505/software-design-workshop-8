e343f68d30e91ccb9fc7c6686fb13f7e3fa07e67
Akella <leosaz0617@gmail.com>
2025-10-23 14:36:12 +0300

    feat: axios set up

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-	baseURL: import.meta.env.VITE_API_BASE_URL,
+	baseURL: process.env['VITE_API_BASE_URL'],
 	headers: {
 		'Content-Type': 'application/json',
 	}
@@ -9,8 +9,8 @@ const apiClient = axios.create({
 
 apiClient.interceptors.response.use(
 	(response) => response,
-	(error) => {
-		console.error('API error', error.response?.data || error.message);
+	(error: Error) => {
+		console.error(`API error ${error.name}: ${error.message}`);
 		return Promise.reject(error);
 	}
 );
